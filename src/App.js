4ef0171b1abc3db662b1d4006f9f9f0223ce4ebe
skinnypete65 <skinnypetetskipe@gmail.com>
2024-01-12 22:28:21 +0300

    add working with posts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css"
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import postList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo"/>
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const postsArr = [];
+    for (let i = 1; i <= 10; i++) {
+        const title = "Java " + i
+        const body = "Some body about Java" + i
+
+        postsArr.push({id: i, title: title, body: body})
+    }
+
+    const [posts, setPosts] = useState(postsArr)
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    return (
+        <div className="App">
+            <PostForm create={createPost}/>
+            <PostList posts={posts} title={"Список постов"} remove={removePost}/>
+        </div>
+    );
 }
+
 
 export default App;
