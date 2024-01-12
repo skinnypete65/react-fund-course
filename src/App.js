508506e3379c4f9cb32260b6b44181dac5ff80c0
skinnypete65 <skinnypetetskipe@gmail.com>
2024-01-12 15:34:06 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo"/>
         <p>
-          Я ЕБАЛ ВАШИХ МАМОК
+          Hello world!
         </p>
         <a
           className="App-link"
